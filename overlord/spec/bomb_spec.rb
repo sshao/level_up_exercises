6c5772bf7b596a6593d7e82e73cabaae4284b912
Sophia Shao <sshao@enova.com>
2014-07-01 16:37:30 -0500

    Refactored spec and bomb after code review
    Spec could probably be further refactored

--- overlord/spec/bomb_spec.rb
+++ overlord/spec/bomb_spec.rb
@@ -5,143 +5,82 @@ describe Bomb do
   let(:default_activation_code) { "1234" }
   let(:default_deactivation_code) { "0000" }
   
+  INVALID_CODES = [
+    {code: "22222", description: ">4 characters"},
+    {code: "12", description: "<4 characters"},
+    {code: "aaaa", description: "all non-number characters"},
+    {code: "a2a2", description: "mixed number/non-number characters"}
+  ]
+  
   describe "#new" do  
     it "sets default activation code to 1234" do
-      expect(bomb.instance_variable_get(:@activation_code)).to eq default_activation_code
+      expect(activation_code_set?(bomb, default_activation_code)).to be true
     end
 
     it "sets default deactivation code to 0000" do
-      expect(bomb.instance_variable_get(:@deactivation_code)).to eq default_deactivation_code
+      expect(deactivation_code_set?(bomb, default_deactivation_code)).to be true
     end
 
     it "sets the bomb state to deactivated" do
-      expect(bomb.state).to eq :deactivated
+      expect(bomb.state).to be :deactivated
     end
   end
 
-  describe "#activation_code" do
-    context "with valid activation code" do
+  describe "#set_activation_code" do
+    context "with valid code" do
       let(:code) { "5555" }
 
       it "correctly sets activation code" do
-        expect{bomb.activation_code(code)}.to change{bomb.instance_variable_get(:@activation_code)}.to("5555")
+        bomb.set_activation_code(code)
+        expect(activation_code_set?(bomb, code)).to be true
       end
 
       it "returns truthy" do
-        expect(bomb.activation_code(code)).to be_truthy
+        expect(bomb.set_activation_code(code)).to be_truthy
       end
     end
     
     context "with invalid activation code" do
-      context "that is >4 characters" do
-        let(:code) { "22222" }
+      INVALID_CODES.each do |code|
+        context "that is #{code[:description]}" do
+          it "does not change the activation code" do
+            bomb.set_activation_code(code[:code])
+            expect(activation_code_set?(bomb, code[:code])).to be false
+          end
 
-        it "does not change the activation code" do
-          expect{bomb.activation_code(code)}.to_not change{bomb.instance_variable_get(:@activation_code)}
-        end
-
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-      
-      context "that is <4 characters" do
-        let(:code) { "12" }
-
-        it "does not change the activation code" do
-          expect{bomb.activation_code(code)}.to_not change{bomb.instance_variable_get(:@activation_code)}
-        end
-
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-
-      context "that is all non-number characters" do
-        let(:code) { "aaaa" }
-
-        it "does not change the activation code" do
-          expect{bomb.activation_code(code)}.to_not change{bomb.instance_variable_get(:@activation_code)}
-        end
-        
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-
-      context "that is mixed number/non-number characters" do
-        let(:code) { "a2a2" }
-
-        it "does not change the activation code" do
-          expect{bomb.activation_code(code)}.to_not change{bomb.instance_variable_get(:@activation_code)}
-        end
-        
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
+          it "returns falsey" do
+            expect(bomb.set_activation_code(code[:code])).to be_falsey
+          end
         end
       end
     end
   end
 
-  describe "#deactivation_code=" do
+  describe "#set_deactivation_code" do
     context "with valid deactivation code" do
       let(:code) { "6666" }
 
       it "correctly sets deactivation code" do
-        expect{bomb.deactivation_code(code)}.to change{bomb.instance_variable_get(:@deactivation_code)}
+        bomb.set_deactivation_code(code)
+        expect(deactivation_code_set?(bomb, code)).to be true
       end
 
       it "returns truthy" do
-        expect(bomb.deactivation_code(code)).to be_truthy
+        expect(bomb.set_deactivation_code(code)).to be_truthy
       end
     end
-
+    
     context "with invalid deactivation code" do
-      context "that is >4 characters" do
-        let(:code) { "66666" }
+      INVALID_CODES.each do |code|
+        context "that is #{code[:description]}" do
+          it "does not change the deactivation code" do
+            bomb.set_deactivation_code(code[:code])
+            expect(deactivation_code_set?(bomb, code[:code])).to be false
+          end
 
-        it "does not change the deactivation code" do
-          expect{bomb.deactivation_code(code)}.to_not change{bomb.instance_variable_get(:@deactivation_code)}
-        end
-        
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-      
-      context "that is <4 characters" do
-        let(:code) { "666" }
-
-        it "does not change the deactivation code" do
-          expect{bomb.deactivation_code(code)}.to_not change{bomb.instance_variable_get(:@deactivation_code)}
-        end
-        
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-
-      context "that is all non-number characters" do
-        let(:code) { "aaaa" }
-
-        it "does not change the deactivation code" do
-          expect{bomb.deactivation_code(code)}.to_not change{bomb.instance_variable_get(:@deactivation_code)}
-        end
-        
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
-        end
-      end
-
-      context "that is mixed number/non-number characters" do
-        let(:code) { "6a6a" }
-
-        it "does not change the deactivation code" do
-          expect{bomb.deactivation_code(code)}.to_not change{bomb.instance_variable_get(:@deactivation_code)}
-        end
-
-        it "returns falsey" do
-          expect(bomb.activation_code(code)).to be_falsey
+          it "returns falsey" do
+            expect(bomb.set_deactivation_code(code[:code])).to be_falsey
+          end
         end
       end
     end
@@ -174,7 +113,7 @@ describe Bomb do
 
     context "activated bomb" do
       before(:each) do
-        bomb.instance_variable_set(:@state, :activated)
+        bomb.activate(default_activation_code)
       end
 
       context "with correct activation code" do
@@ -202,7 +141,8 @@ describe Bomb do
 
     context "exploded bomb" do
       before(:each) do
-        bomb.instance_variable_set(:@state, :exploded)
+        bomb.activate(default_activation_code)
+        3.times{bomb.deactivate("aaaa")}
       end
 
       context "with correct activation code" do
@@ -232,7 +172,7 @@ describe Bomb do
   describe "#deactivate" do
     context "activated bomb" do 
       before(:each) do
-        bomb.instance_variable_set(:@state, :activated)
+        bomb.activate(default_activation_code)
       end
 
       context "with correct deactivation code" do
@@ -267,10 +207,6 @@ describe Bomb do
     end
 
     context "deactivated bomb" do 
-      before(:each) do
-        allow(bomb).to receive(:state) { :deactivated }
-      end
-
       context "with correct deactivation code" do
         it "has no effect on bomb state" do
           expect{bomb.deactivate(default_deactivation_code)}.to_not change{bomb.state}
@@ -296,7 +232,8 @@ describe Bomb do
     
     context "exploded bomb" do
       before(:each) do
-        bomb.instance_variable_set(:@state, :exploded)
+        bomb.activate(default_activation_code)
+        3.times{bomb.deactivate("aaaa")}
       end
 
       context "with correct activation code" do
--- overlord/spec/spec_helper.rb
+++ overlord/spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require_relative "../models/bomb"
+require 'helpers'
 
+RSpec.configure do |c|
+  c.include Helpers
+end
