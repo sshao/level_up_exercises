e48785b0024e62b2bbb08539240dd357d1dc75b7
Sophia Shao <sshao@enova.com>
2014-07-21 17:29:55 -0500

    Started implementation of "Favorite" feature
    * Added a "Favorite" button to PaletteSet#show view
    * Started a UsersController#favorite method
    * Added POST /favorite/:id to routes
      * id is the id of the PaletteSet to be favorited

---
+++ colors/app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def favorite
+  end
+end
--- colors/config/routes.rb
+++ colors/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/favorite'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
   # FIXME not using all the resources... using just for url helper
   resources :palette_sets
   post 'generate' => 'palette_sets#create'
+  post 'favorite/:id' => 'users#favorite'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
