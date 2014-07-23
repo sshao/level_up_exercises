22a02ff104eaafeaadab85d6af7aedce82b6d1a8
Sophia Shao <sshao@enova.com>
2014-07-23 10:41:04 -0500

    Remove non-error flash from PaletteSetsController
    * Remove flash[:alert] = "No photo posts foudn to generate palettes
      from" out of PaletteSetsController, and from the flash messages in
      general
      * Instead display the message in the body of the PaletteSet#show view,
        in lieu of palettes

--- colors/app/controllers/palette_sets_controller.rb
+++ colors/app/controllers/palette_sets_controller.rb
@@ -3,16 +3,12 @@ class PaletteSetsController < ApplicationController
 
   def create
     palette_set = fetch_or_return(palette_set_params)
-
     flash[:error] = palette_set.errors.full_messages
-    # FIXME move into view
-    flash[:alert] = "No photo posts found to generate palettes from" if palette_set.palettes.empty?
-
     respond_with(palette_set)
   end
 
-  # FIXME look at scopes (recent, popular)
   def index
+    # TODO use scopes (recent, popular)
     @palette_sets = PaletteSet.all
   end
 
