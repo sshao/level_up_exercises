e293b768f3095a650e8b83d7ebbfb6a6fb401444
Sophia Shao <sshao@enova.com>
2014-07-25 11:51:12 -0500

    Refactored PaletteSets#show view
    * Main bit of refactoring: PaletteSetsHelper#favorite_button

--- colors/app/helpers/palette_sets_helper.rb
+++ colors/app/helpers/palette_sets_helper.rb
@@ -1,2 +1,16 @@
 module PaletteSetsHelper
+  def user_area(&block)
+    yield if user_signed_in?
+  end
+
+  def tumblr_url(source)
+    "http://#{source}.tumblr.com"
+  end
+
+  def favorite_button(disabled)
+    button_params = { true => { state: "Favorited", classes: "button tiny round disabled" },
+      false => { state: "Favorite", classes: "button tiny round" } }
+
+    yield(button_params[disabled][:state], button_params[disabled][:classes], disabled)
+  end
 end
