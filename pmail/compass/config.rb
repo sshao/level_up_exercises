fda2bbcdd099d763b38802632b48ba19bed43ed8
Sophia Shao <sshao@enova.com>
2014-07-28 15:44:22 -0500

    Add foundation to site
    * Sorta hacked Rules file to get it working... needs major fixing

--- pmail/compass/config.rb
+++ pmail/compass/config.rb
@@ -1,8 +1,15 @@
+require 'zurb-foundation'
+
 http_path    = "/"
 project_path = "."
-css_dir      = "output/assets/style"
-sass_dir     = "content/assets/style"
-images_dir   = "output/assets/images"
+css_dir      = "output/assets"
+images_dir   = "content/assets/images"
+javascripts_dir = "content/assets/javascripts"
+sass_dir     = "content/assets/stylesheets"
+
+output_style = :compressed
+preferred_syntax = :scss
+relative_assets = true
 
 # when using SCSS:
 sass_options = {
